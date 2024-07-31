d96e7d8adcb146a846d75683a3059a489aaa1783
frank <ndungufrank01@gmail.com>
2024-07-31 19:28:57 +0300

    code refactoring

--- cargosai-frontend/views/Home.vue
+++ cargosai-frontend/views/Home.vue
@@ -22,9 +22,9 @@
       <h2>As featured in</h2>
       <div class="hero-gallery">
         <img
-          src="https://res.cloudinary.com/kwishi/image/upload/v1722436613/indiehackers_ww9uac.svg"
-          alt="Indie Hackers logo"
-          class="indie-hackers-logo"
+          src="https://res.cloudinary.com/kwishi/image/upload/v1722437667/1658832030reddit-logo-png_vjgjzf.svg"
+          alt="Reddit logo"
+          class="reddit-logo"
         />
         <img
           src="https://res.cloudinary.com/kwishi/image/upload/v1722436863/product-hunt-logo-horizontal-black_sipadd.svg"
@@ -32,9 +32,9 @@
           class="product-hunt-logo"
         />
         <img
-          src="https://res.cloudinary.com/kwishi/image/upload/v1722437667/1658832030reddit-logo-png_vjgjzf.svg"
-          alt="Reddit logo"
-          class="reddit-logo"
+          src="https://res.cloudinary.com/kwishi/image/upload/v1722436613/indiehackers_ww9uac.svg"
+          alt="Indie Hackers logo"
+          class="indie-hackers-logo"
         />
       </div>
     </div>
