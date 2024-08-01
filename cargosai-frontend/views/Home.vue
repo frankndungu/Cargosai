49e98857d6c4c7ada6f3ec974f17861f07eedb0e
frank <ndungufrank01@gmail.com>
2024-08-01 14:25:50 +0300

    featured products

--- cargosai-frontend/views/Home.vue
+++ cargosai-frontend/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="hero section">
+  <section class="hero">
     <div class="hero-content">
       <h1>
         Get beautiful, <em class="em">handmade antiques</em> from Kenya,<br />
@@ -39,10 +39,75 @@
       </div>
     </div>
   </section>
-
-  <section class="hero-products">
-    <div>featured products</div>
+  <section class="featured-products">
+    <h2 class="featured-heading">Featured Products</h2>
+    <p class="featured-text">
+      We haven't launched yet, but we are accepting pre-orders.
+    </p>
+    <div class="product-grid">
+      <ProductCard
+        v-for="product in products"
+        :key="product.id"
+        :product="product"
+      />
+    </div>
   </section>
 </template>
 
-<script setup></script>
+<script setup>
+import ProductCard from "@/components/ProductCard.vue";
+
+const products = [
+  {
+    id: 1,
+    name: "Maasai Beaded Necklace",
+    description: "Handcrafted necklace with traditional Maasai beads.",
+    price: "$25",
+    image:
+      "https://res.cloudinary.com/kwishi/image/upload/v1722319334/hero-jewelry_xdtiga.jpg",
+  },
+  {
+    id: 2,
+    name: "Maasai Beaded Necklace",
+    description: "Handcrafted necklace with traditional Maasai beads.",
+    price: "$25",
+    image:
+      "https://res.cloudinary.com/kwishi/image/upload/v1722319334/hero-jewelry_xdtiga.jpg",
+  },
+
+  {
+    id: 3,
+    name: "Maasai Beaded Necklace",
+    description: "Handcrafted necklace with traditional Maasai beads.",
+    price: "$25",
+    image:
+      "https://res.cloudinary.com/kwishi/image/upload/v1722319334/hero-jewelry_xdtiga.jpg",
+  },
+
+  {
+    id: 4,
+    name: "Maasai Beaded Necklace",
+    description: "Handcrafted necklace with traditional Maasai beads.",
+    price: "$25",
+    image:
+      "https://res.cloudinary.com/kwishi/image/upload/v1722319334/hero-jewelry_xdtiga.jpg",
+  },
+  {
+    id: 5,
+    name: "Maasai Beaded Necklace",
+    description: "Handcrafted necklace with traditional Maasai beads.",
+    price: "$25",
+    image:
+      "https://res.cloudinary.com/kwishi/image/upload/v1722319334/hero-jewelry_xdtiga.jpg",
+  },
+
+  {
+    id: 6,
+    name: "Maasai Beaded Necklace",
+    description: "Handcrafted necklace with traditional Maasai beads.",
+    price: "$25",
+    image:
+      "https://res.cloudinary.com/kwishi/image/upload/v1722319334/hero-jewelry_xdtiga.jpg",
+  },
+];
+</script>
