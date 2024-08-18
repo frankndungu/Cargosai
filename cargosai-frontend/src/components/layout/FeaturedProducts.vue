dcdb768d3339ddcab07ef9e7a9df63b4002a83b8
frank <ndungufrank01@gmail.com>
2024-08-18 13:18:25 +0300

    prices

--- cargosai-frontend/src/components/layout/FeaturedProducts.vue
+++ cargosai-frontend/src/components/layout/FeaturedProducts.vue
@@ -55,8 +55,8 @@ const products = [
   {
     id: 5,
     name: "Chic Beaded Wall Mirror",
-    description: "Adding a touch of elegance",
-    price: "$65.00",
+    description: "Adding a touch of elegance (Set of 3)",
+    price: "$230.00",
     image:
       "https://res.cloudinary.com/kwishi/image/upload/v1723444931/IMG_1878_ztwfgq.jpg",
   },
@@ -67,6 +67,33 @@ const products = [
     price: "$55.00",
     image:
       "https://res.cloudinary.com/kwishi/image/upload/v1723441934/IMG_1887_cndofb.jpg",
+  },
+  {
+    id: 7,
+    name: "Beaded Bracelets",
+    description:
+      "Perfect for adding a touch of tradition to any outfit (Set of 5)",
+    price: "$75.95",
+    image:
+      "https://res.cloudinary.com/kwishi/image/upload/v1723876517/C08AABD9-0736-4550-84D2-397DCAC36007_gzwims.jpg",
+  },
+  {
+    id: 8,
+    name: "Brass Bracelets",
+    description:
+      "Handcrafted brass bracelet with intricate detailing (Set of 5)",
+    price: "$75.95",
+    image:
+      "https://res.cloudinary.com/kwishi/image/upload/v1723876880/A08FAA78-332F-4BC8-B5B9-91C2AA28B634_pu9wfi.jpg",
+  },
+  {
+    id: 9,
+    name: "Earrings",
+    description:
+      "Each uniquely designed to elevate your style with a touch of artisanal flair (Set of 5)",
+    price: "$65.95",
+    image:
+      "https://res.cloudinary.com/kwishi/image/upload/v1723877597/IMG_1868_pz49ft.jpg",
   },
 ];
 </script>
