af77bd92c63a90cafe8b63bb4c1ccd29f530ed11
frank <ndungufrank01@gmail.com>
2024-09-11 10:49:03 +0300

    added sanity credentials to .env

--- maasaimarketblog/sanity.config.js
+++ maasaimarketblog/sanity.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'MAASAIMARKETBLOG',
 
-  projectId: '0itxu2je',
-  dataset: 'production',
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
 
   plugins: [structureTool(), visionTool()],
 
