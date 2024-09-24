c8dfbd50d8b6087995ebd10598456ceeb7b413e0
frank <ndungufrank01@gmail.com>
2024-09-24 21:06:19 +0300

    some database changes

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Create 10 unique users using a factory
         User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
+        // Create a unique test user only if it doesn't already exist
+        User::firstOrCreate([
             'email' => 'test@example.com',
+        ], [
+            'name' => 'Test User',
+            'password' => bcrypt('password'), // Ensure to set a password
         ]);
 
         // Call the ProductSeeder to seed products
--- server/routes/api.php
+++ server/routes/api.php
@@ -1,15 +1,15 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Product Routes
 Route::get('/products', [ProductController::class, 'index']); // all products
-
-Route::get('products/{slug}', [ProductController::class, 'show']); //individual product by slug
-
+Route::get('products/{id}', [ProductController::class, 'show']); // route individual product by ID
+Route::get('products/slug/{slug}', [ProductController::class, 'showBySlug']); // route individual product by slug for product overview
 Route::post('products', [ProductController::class, 'store']);
-
 
 Route::get('/user', function (Request $request) {
     return $request->user();
