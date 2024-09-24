b3e73ca53e6392653c387eafea528fd6fbd2318a
frank <ndungufrank01@gmail.com>
2024-09-24 10:52:29 +0300

    modal validation

--- client/src/components/ui/ReviewModal.vue
+++ client/src/components/ui/ReviewModal.vue
@@ -11,7 +11,7 @@
         <!-- Name Field -->
         <div class="modal-form-group">
           <label for="name">Name</label>
-          <input type="text" id="name" v-model="review.name" required />
+          <input type="text" id="name" v-model="review.name" />
           <span v-if="errors.name" class="error-msg">{{ errors.name }}</span>
         </div>
 
@@ -35,19 +35,14 @@
         <!-- Review Title Field -->
         <div class="modal-form-group">
           <label for="title">Review Title</label>
-          <input type="text" id="title" v-model="review.title" required />
+          <input type="text" id="title" v-model="review.title" />
           <span v-if="errors.title" class="error-msg">{{ errors.title }}</span>
         </div>
 
         <!-- Review Content Field -->
         <div class="modal-form-group">
           <label for="review">Review</label>
-          <textarea
-            id="review"
-            v-model="review.content"
-            rows="4"
-            required
-          ></textarea>
+          <textarea id="review" v-model="review.content" rows="4"></textarea>
           <span v-if="errors.content" class="error-msg">{{
             errors.content
           }}</span>
@@ -63,7 +58,7 @@
 
         <!-- Terms and Conditions -->
         <p class="terms-text">
-          By publishing this review, you agree with the
+          By publishing this review, you agree with our
           <router-link to="/terms-of-service" class="terms-link"
             >terms and conditions</router-link
           >
@@ -100,6 +95,7 @@ const closeModal = () => {
 };
 
 const submitReview = () => {
+  // Trigger validation when the user clicks "Add Review"
   if (validateForm()) {
     console.log("Review submitted:", review.value);
     closeModal();
@@ -110,18 +106,25 @@ const validateForm = () => {
   let isValid = true;
   errors.value = { name: null, rating: null, title: null, content: null };
 
+  // Name validation
   if (!review.value.name) {
     errors.value.name = "Name is required.";
     isValid = false;
   }
+
+  // Rating validation
   if (!review.value.rating) {
     errors.value.rating = "Please select a rating.";
     isValid = false;
   }
+
+  // Title validation
   if (!review.value.title) {
     errors.value.title = "Review title is required.";
     isValid = false;
   }
+
+  // Content validation
   if (!review.value.content) {
     errors.value.content = "Please write your review.";
     isValid = false;
