d575d3c8d3bde47b6992f8dd7bceb1a05bcde116
frank <ndungufrank01@gmail.com>
2024-12-24 22:01:22 +0300

    removed shipping_fee from payments_table

---
+++ server/database/migrations/2024_12_24_181647_add_shipping_fee_to_payments_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('payments', function (Blueprint $table) {
+            $table->decimal('shipping_fee', 10, 2)->nullable(); // Add shipping_fee column
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('payments', function (Blueprint $table) {
+            $table->dropColumn('shipping_fee');
+        });
+    }
+};
