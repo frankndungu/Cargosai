f983c29cb9ff93ca8d86d45a19444124e0886cdc
frank <ndungufrank01@gmail.com>
2025-01-03 08:42:22 +0300

    allowed origins cors

--- server/config/cors.php
+++ server/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://maasaimarketonline.com/', 'http://www.maasaimarketonline.com/'],
+    'allowed_origins' => ['http://localhost:3000', 'https://maasaimarketonline.com/', 'https://www.maasaimarketonline.com/'],
 
     'allowed_origins_patterns' => [],
 
