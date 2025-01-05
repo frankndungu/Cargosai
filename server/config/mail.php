eeabf46981ec038d397bd02e005b1a635d988b76
frank <ndungufrank01@gmail.com>
2025-01-05 12:03:15 +0300

    mailers

--- server/config/mail.php
+++ server/config/mail.php
@@ -37,14 +37,14 @@ return [
 
     'mailers' => [
         'brevo' => [
-            'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp-relay.brevo.com'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+        'transport' => 'smtp',
+        'host' => env('MAIL_HOST', 'smtp-relay.brevo.com'),
+        'port' => env('MAIL_PORT', 587),
+        'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+        'username' => env('MAIL_USERNAME'),
+        'password' => env('MAIL_PASSWORD'),
+        'timeout' => null,
+        'local_domain' => 'maasaimarketonline.com', 
         ],
 
         'smtp' => [
